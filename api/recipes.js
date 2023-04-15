b3b2f4edaf33a6dfb14ef485162c3bc7112086dc
Harry McAlister <harry.mcalist@gmail.com>
2023-04-15 22:12:03 +0100

    Fix bugs, clean endpoint and set appropriate rate limiting

--- api/recipes.js
+++ api/recipes.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const redis = require("ioredis");
+const Redis = require("ioredis");
 
 const redis = new Redis(process.env.UPSTASH_REDIS_URL);
 const BASE_URL = "https://tasty.p.rapidapi.com";
@@ -11,22 +11,30 @@ const headers = {
 };
 
 const WINDOW_MS = 10 * 1000; // 10 seconds
-const MAX_REQUESTS = 1;
+const MAX_REQUESTS = 5;
 
 const rateLimiter = async (req, res, next) => {
-  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  const key = `rate-limit:${ip}`;
+  try {
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    const key = `rate-limit:${ip}`;
 
-  const current = await redis.get(key);
+    console.log('IP:', ip);
 
-  if (current === null || current < MAX_REQUESTS) {
-    await redis.multi()
-      .incr(key)
-      .expire(key, WINDOW_MS / 1000)
-      .exec();
-    next();
-  } else {
-    res.status(429).send({ error: "Too many requests, please try again later." });
+    const current = await redis.get(key);
+    console.log('Current:', current);
+
+    if (current === null || current < MAX_REQUESTS) {
+      await redis.multi()
+        .incr(key)
+        .expire(key, WINDOW_MS / 1000)
+        .exec();
+      next();
+    } else {
+      res.status(429).send({ error: "Too many requests, please try again later." });
+    }
+  } catch (error) {
+    console.log("RateLimiter error:", error);
+    res.status(500).send({ error: `An error occurred in the rate limiter: ${error.message}` });
   }
 };
 
@@ -43,11 +51,15 @@ module.exports = async (req, res) => {
     url.searchParams.append("q", queryParams.search);
 
     try {
+
+      await redis.ping();
+      console.log('Redis connection successful');
+
       const axiosResponse = await axios.get(url.toString(), { headers });
       res.status(200).send(axiosResponse.data);
     } catch (error) {
       console.log("Error:", error);
-      res.status(500).send({ error: "An error occurred." });
+      res.status(500).send({ error: `An error occurred: ${error.message}` });
     }
   });
 };
